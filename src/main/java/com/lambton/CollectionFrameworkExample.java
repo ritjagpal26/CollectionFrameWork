350c5c9c687faa4c9f12226dd3639163866c488b
macstudent <ritikhjagpal@gmail.com>
2019-09-24 18:29:10 -0400

    commit

--- src/main/java/com/lambton/CollectionFrameworkExample.java
+++ src/main/java/com/lambton/CollectionFrameworkExample.java
@@ -1,6 +1,7 @@
 package com.lambton;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CollectionFrameworkExample
 {
@@ -13,11 +14,22 @@ public class CollectionFrameworkExample
         mStringArrayList.add("D");
         mStringArrayList.add("A");
         mStringArrayList.add("A");
-        for (int i=0; i<mStringArrayList.size(); i++)
+        /*for (int i=0; i<mStringArrayList.size(); i++)
         {
             System.out.println("Elements :"+mStringArrayList.get(i));
         }
+        for (String num:mStringArrayList)
+        {
+            System.out.println(num);
+        }*/
+        //mStringArrayList.iterator()
 
+        Iterator<String> iterator=mStringArrayList.iterator();
+        while (iterator.hasNext())
+
+        {
+            String s=iterator.next();
+            System.out.println(s);
+        }
     }
-
 }
