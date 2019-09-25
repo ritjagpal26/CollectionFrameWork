4304b81261c6cf93cdfdae7eb2c5f6fe6d2727ef
macstudent <ritikhjagpal@gmail.com>
2019-09-24 21:05:22 -0400

    commit

--- src/main/java/com/lambton/CollectionFrameworkExample.java
+++ src/main/java/com/lambton/CollectionFrameworkExample.java
@@ -1,5 +1,8 @@
 package com.lambton;
 
+import javafx.collections.transformation.SortedList;
+import sun.applet.resources.MsgAppletViewer;
+
 import java.util.*;
 
 public class CollectionFrameworkExample
@@ -23,15 +26,13 @@ public class CollectionFrameworkExample
         }*/
         //mStringArrayList.iterator()
 
-        Iterator<String> iterator=mStringArrayList.iterator();
-        while (iterator.hasNext())
-
-        {
-            String s=iterator.next();
+        Iterator<String> iterator = mStringArrayList.iterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
             System.out.println(s);
         }
 
-        Set<String> mySet=new HashSet<>();
+        Set<String> mySet = new HashSet<>();
 
         mySet.add("India");
         mySet.add("USA");
@@ -40,7 +41,7 @@ public class CollectionFrameworkExample
         mySet.add("Pakistan");
         System.out.println(mySet);
 
-        Map<String, String> stringStringMap=new HashMap<>();
+        Map<String, String> stringStringMap = new HashMap<>();
         stringStringMap.put("IND", "India");
         stringStringMap.put("CAD", "Canada");
         stringStringMap.put("USA", "United State");
@@ -49,29 +50,74 @@ public class CollectionFrameworkExample
         stringStringMap.put("CAD", "Canada");
         System.out.println(stringStringMap);
 
-        ArrayList <String> statesOfIndia=new ArrayList<>();
+        ArrayList<String> statesOfIndia = new ArrayList<>();
 
         statesOfIndia.add("PB");
         statesOfIndia.add("HP");
         statesOfIndia.add("AP");
 
-        ArrayList <String> statesOfUsa=new ArrayList<>();
+        ArrayList<String> statesOfUsa = new ArrayList<>();
 
         statesOfUsa.add("NY");
         statesOfUsa.add("CA");
         statesOfIndia.add("NJ");
 
-        ArrayList <String> statesOfCAD=new ArrayList<>();
+        ArrayList<String> statesOfCAD = new ArrayList<>();
         statesOfCAD.add("ON");
         statesOfCAD.add("SK");
 
 
-        Map<String,ArrayList > stringStringMap1=new HashMap<>();
+        Map<String, ArrayList> stringStringMap1 = new HashMap<>();
 
-        stringStringMap1.put("IND",statesOfIndia);
-        stringStringMap1.put("USA",statesOfUsa);
-        stringStringMap1.put("CAD",statesOfCAD);
-        System.out.println(stringStringMap1);
+        stringStringMap1.put("IND", statesOfIndia);
+        stringStringMap1.put("USA", statesOfUsa);
+        stringStringMap1.put("CAD", statesOfCAD);
+        System.out.println(stringStringMap1.get("CAD"));
 
-    }
-}
+        ArrayList<String> checkData = new ArrayList<>();
+
+        checkData.add("abc");
+        checkData.add("cab");
+        checkData.add("cat");
+        checkData.add("mate");
+        checkData.add("Atem");
+        checkData.add("bac");
+        checkData.add("tac");
+        checkData.add("atc");
+        checkData.add("pop");
+        checkData.add("pat");
+        checkData.add("123");
+        checkData.add("231");
+        checkData.add("431");
+
+        /*for (int i=0;i>checkData.size();i++)
+        {
+
+            if (checkData.equals(i) == Character.isLetter(c)) {
+                System.out.println(checkData.get(i));
+            }
+        }
+        char a = 0;
+        for (int i = 0; i < checkData.size(); i++) {
+            if (Character.isLetter(a) && a == 'a' && a == 'c' && a == 'b') {
+                Character.isLetter(a);
+                Character.valueOf(checkData.get(i))= Character.isLetter(a);
+                System.out.println(checkData.get(i));
+            }*/
+        Map<String ,ArrayList> map=new HashMap<>();
+        for (String s:checkData) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String t = new String(c);
+            ArrayList<String> al = map.get(t);
+            if (al == null) {
+                al = new ArrayList<>();
+            }
+            al.add(s);
+            map.put(t, al);
+        }
+            System.out.println(map);
+        }
+        }
+
+
